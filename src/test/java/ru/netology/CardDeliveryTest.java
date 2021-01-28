b38654d443d0ee8e8b20e5c5194fbf8e6df3a657
Elena Fedoseeva <horungeelena@gmail.com>
2021-01-28 23:15:32 +0200

    Updated test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,9 +1,11 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
@@ -11,20 +13,21 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.*;
 
 class CardDeliveryTest {
+    String inputDate = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
     @Test
     void shouldRegisterByDelivery() {
         open("http://localhost:9999");
-        SelenideElement form = $("form");
+        //SelenideElement form = $("form");
         $("[data-test-id=city] input").setValue("Калининград");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue("11.02.2021");
+        $("[data-test-id=date] input").setValue(inputDate);
         $("[data-test-id=name] input").setValue("Анна Петрова");
         $("[data-test-id=phone] input").setValue("+79111234567");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $(withText("Успешно")).waitUntil(Condition.visible, 15000);
-        $(withText("Встреча успешно забронирована на 11.02.2021"));
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно забронирована на " + inputDate));
     }
 }
 
