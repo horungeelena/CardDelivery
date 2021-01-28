85a655ac348785162fb8c2c2835acd8b071a21ef
Elena Fedoseeva <horungeelena@gmail.com>
2021-01-28 19:18:29 +0200

    Update

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
@@ -16,13 +17,14 @@ class CardDeliveryTest {
         open("http://localhost:9999");
         SelenideElement form = $("form");
         $("[data-test-id=city] input").setValue("Калининград");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue("11.02.2021");
         $("[data-test-id=name] input").setValue("Анна Петрова");
         $("[data-test-id=phone] input").setValue("+79111234567");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
         $(withText("Успешно")).waitUntil(Condition.visible, 15000);
-        $(withText("Встреча успешно забронирована на 29.01.2021"));
+        $(withText("Встреча успешно забронирована на 11.02.2021"));
     }
 }
 
